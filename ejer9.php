e44acb4e25797e3f63c94bca620c0522e4d598af
Adrben <admobe@alumnatflorida.es>
2025-10-27 17:43:28 +0100

    Hasta ejer10

--- ejer9.php
+++ ejer9.php
@@ -1,5 +1,20 @@
 <?php
 
-include "data.php"
+include "data.php";
+$contNo=0;
+$edadNo=0;
+
+foreach($data as $datos){
+
+    if($datos["Favorites"] == ""){
+
+        $contNo++;
+        $edadNo+=$datos["Age"];
+        
+    }
+}
+
+echo "Total people with no preferences: $contNo";
+echo "<br> Average age for people with no preferences: " . round($edadNo/$contNo) . "<br><br>";
 
 ?>
