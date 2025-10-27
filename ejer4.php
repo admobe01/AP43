383136f29246177d09aa2f84c2cd1a5f0b518648
Adrben <admobe@alumnatflorida.es>
2025-10-27 10:04:40 +0100

    ejer4

--- ejer4.php
+++ ejer4.php
@@ -1,10 +1,27 @@
 <?php
 
-$city = "Tokyo,Mexico City,New York City,Mumbai,Seoul,Shanghai,Lagos,Buenos Aires, Cairo,London";
+$city = "Tokyo,Mexico City,New York City,Mumbai,Seoul,Shanghai,Lagos,Buenos Aires,Cairo,London";
 $country = "Japan,Mexico,USA,India,Korea,China,Nigeria,Argentina,Egypt,England";
 
 $array1 = explode(",",$city);
 $array2 = explode(",", $country);
-var_dump($array1);
-var_dump($array2);
+$array3=[];
+/*var_dump($array1);
+var_dump($array2);*/
+
+if(count($array1)==count($array2)){
+
+
+for($i=0;$i<count($array1);$i++){
+
+    $array3[$i] = ['country' => $array2[$i], 'city'=> $array1[$i]]; 
+
+}
+
+}else{
+
+    echo "Error en la longitud de Arrays";
+}
+
+var_dump($array3)
 ?>
