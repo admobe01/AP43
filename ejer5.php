95c37c7158442a65463c9e74bd26a2e724dba62b
Adrben <admobe@alumnatflorida.es>
2025-10-27 15:40:33 +0100

    ejer5.3

--- ejer5.php
+++ ejer5.php
@@ -1,19 +1,23 @@
 <?php
 
+$array2=[];
+$array=[];
+
 $data = "Tokyo, Japan; Mexico City, Mexico; New York City, USA; Mumbai, India; Seoul, Korea; Shanghai, China; Lagos, Nigeria; Buenos Aires, Argentina; Cairo, Egypt; London, England";
 
 $array0 = explode(";", $data);
 $array1 = implode(",", $array0);
 $array2 = explode(",", $array1);
+$j=0;
+$array=[];
 
-$array3=[];
-
-for($i=0;$i<count($array2);$i=$i+2)//hay que mirar porque el array salta un numero
-{
+for($i=0;$i<count($array2)/2;$i++){
 
 
-        $array[$i] = [ 'country' => $array2[$i+1], 'city' => $array2[$i]];
+        $array[$i] = [ 'country' => $array2[$j+1], 'city' => $array2[$j]];
 
+        $j=$j+2;
+        
 }
 
 var_dump($array);
