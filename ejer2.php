66929b6fb21756ca7ac2038768ba89de691bbafc
Adrben <admobe@alumnatflorida.es>
2025-10-27 09:36:24 +0100

    ejer3

--- ejer2.php
+++ ejer2.php
@@ -1,3 +1,22 @@
 <?php
 
+
+$country = "Japan,Mexico,USA,India,Korea,China,Nigeria,Argentina,Egypt,England";
+
+$capitals= [
+
+    ['country' => "Japan"],
+    ['country' => "Mexico"],
+    ['country' => "USA"],
+    ['country' => "India"],
+    ['country' => "Korea"],
+    ['country' => "China"],
+    ['country' => "Nigeria"],
+    ['country' => "Argentina"],
+    ['country' => "Egypt"],
+    ['country' => "England"]
+];
+
+var_dump($capitals)
+
 ?>
---
+++ ejer3.php
@@ -0,0 +1,49 @@
+<?php
+
+$capitals = [
+
+    [
+        'country' => "Japan",
+        'capital' => "Tokyo"
+    ],
+    [
+        'country' => "Mexico",
+        'capital' => "Mexico City"
+
+    ],
+    [
+        'country' => "USA",
+        'city' => "New York City"
+    ],
+    [
+        'country' => "India",
+        'city' => "Mumbai"
+    ],
+    [
+        'country' => "Korea",
+        'city' => "Seoul"
+    ],
+    [
+        'country' => "China",
+        'city' => "Shangai"
+    ],
+    [
+        'country' => "Nigeria",
+        'city' => "Lagos"
+    ],
+    [
+        'country' => "Argentina",
+        'city' => "Buenos Aires"
+    ],
+    [
+        'country' => "Egypt",
+        'city' => "Cairo"
+    ],
+    [
+        'country' => "England",
+        'city' => "London"
+    ]
+    ];
+
+    var_dump($capitals)
+?>
