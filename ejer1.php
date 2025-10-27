45f1082d8e4072d329170ed8848f42aea73171a6
Adrben <admobe@alumnatflorida.es>
2025-10-27 09:18:24 +0100

    Dos

--- ejer1.php
+++ ejer1.php
@@ -1,5 +1,22 @@
 <?php
 
+$city = "Tokyo,Mexico City,New York City,Mumbai,Seoul,Shanghai,Lagos,Buenos Aires, Cairo,London";
 
+$capital=[
+
+        ['city'=>"Tokio"],
+        ['city'=>"Mexico City"],
+        ['city'=>"New York City"],
+        ['city'=>"Mumbai"],
+        ['city'=>"Seoul"],
+        ['city'=>"Shangai"],
+        ['city'=>"Lagos"],
+        ['city'=>"Buenos Aires"],
+        ['city'=>"Cairo"],
+        ['city'=>"London"],
+    ];
+
+
+var_dump($capital)
 
 ?>
